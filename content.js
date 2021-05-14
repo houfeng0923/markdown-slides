5e35b8a02a509ce8f093477ce5598ecf1657a1ff
houfeng <houfeng@nextop.asia>
2021-05-14 12:23:35 +0800

    fix window blocked

--- content.js
+++ content.js
@@ -8,17 +8,25 @@
       var url = document.location.href.split(/[\#\?]/, 2)[0];
       var fileName = url.split('/').slice(-1)[0];
       var slidesCss = url.slice(0, -fileName.length) + 'slides.css';
-
+      var content = '<html>' +
+      '<head><meta charset="utf-8"><title>' + fileName + '</title></head><body>' +
+      '<link rel="stylesheet" href="' + chrome.extension.getURL('css/slides.css') + '">' +
+      '<link rel="stylesheet" href="' + slidesCss + '">' +
+      '<link rel="stylesheet" href="' + url.replace('.md', '.css') + '">' +
+      '<pre id="source">' + pre.innerHTML + '</pre>' +
+      '<script src="' + chrome.extension.getURL('js/remark.js') + '"></script>' +
+      '<script src="' + chrome.extension.getURL('js/init.js') + '"></script>' +
+      '</body></html>';
       var win = window.open('', fileName, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=' + (screen.width) + ', height=' + (screen.height) + ', top=0, left=0');
-      win.document.write('<html>' +
-        '<head><title>' + fileName + '</title></head><body>' +
-        '<link rel="stylesheet" href="' + chrome.extension.getURL('css/slides.css') + '">' +
-        '<link rel="stylesheet" href="' + slidesCss + '">' +
-        '<link rel="stylesheet" href="' + url.replace('.md', '.css') + '">' +
-        '<pre id="source">' + pre.innerHTML + '</pre>' +
-        '<script src="' + chrome.extension.getURL('js/remark.js') + '"></script>' +
-        '<script src="' + chrome.extension.getURL('js/init.js') + '"></script>' +
-        '</body></html>');
+      try {
+        win.focus();
+        win.document.write(content);
+      } catch(e) {
+        // blocked
+        var file = new Blob( [ content ], { type: 'text/html' } );
+        var url = URL.createObjectURL( file );
+        location.href = url;
+      }
     }
   }
 
